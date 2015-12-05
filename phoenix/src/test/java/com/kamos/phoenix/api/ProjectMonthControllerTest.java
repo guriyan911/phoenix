53a836bfa4df9a95d5eac5070f6121300ad348d4
guriyan911 <yanotatsuya@192.168.10.104>
2015-12-05 11:23:45 +0900

    テストクラスを確認する

---
+++ phoenix/src/test/java/com/kamos/phoenix/api/ProjectMonthControllerTest.java
@@ -0,0 +1,14 @@
+package com.kamos.phoenix.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProjectMonthControllerTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
