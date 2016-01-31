726751391ae21486d7e21bc985a01059321d2fc9
guriyan911 <yanotatsuya@192.168.10.100>
2016-01-31 23:47:55 +0900

    仕組みはまだ理解できていないがログインはSpringBootのサイトの記述通りでできた。 本のようなパスワードのエンコーディング、UserのDBとの突き合わせなどはまだ。

--- phoenix/src/main/java/com/kamos/phoenix/SecurityConfig.java
+++ phoenix/src/main/java/com/kamos/phoenix/SecurityConfig.java
@@ -12,46 +12,40 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// 特定のリクエストに対してセキュリティ設定を無視する
-		web.ignoring().antMatchers("webjars/**", "/css/**", "/images/**");
-	}
-	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	// 認可に関する設定
-    	// loginFormには全てのユーザーのアクセスを可能とする。
-    	// その他のパスには認証なしではアクセスを不可とする。
-        http.authorizeRequests()
-        .antMatchers("loginForm").permitAll()
-        .anyRequest().authenticated();
-        // ログインに関する設定
-        // フォーム認証を有効にし、認証処理のパス、ログインフォームのパス、認証失敗時の遷移先、
-        // 認証成功時の遷移先、ユーザー名、パスワードのパラメータ名を設定する。
-        http.formLogin()
-        .loginProcessingUrl("/login")
-        .loginPage("/loginForm")
-        .failureUrl("loginForm?error")
-        .defaultSuccessUrl("/home",true)
-        .usernameParameter("username").passwordParameter("password")
-        .and();
-        // ログアウトに関する設定
-        http.logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout**"))
-        .logoutSuccessUrl("/loginForm");
-        
-        // その他CSRFなどについても設定が必要になるかも
-        // TODO: put CSRF protection back into this endpoint
-//        .csrf()
-//            .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
-//            .disable()
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home", "/css/**", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/hello", true)
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // Required to use GET method for logout
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(new StandardPasswordEncoder());
+        auth
+        	.inMemoryAuthentication()
+        		.withUser("yano").password("pass").roles("USER");
     }
     
     @Configuration
